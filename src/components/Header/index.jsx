432fb786a2714aab78c211b90adf1ec99eef0d14
Lara Smadeski <lasmadeski@gmail.com>
2024-07-15 16:05:56 +0100

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import { RiShutDownLine } from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
+import { Container, Profile, Logout } from './styles';
+
+export function Header() {
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img
+          src="https://github.com/LaraSmadeski.png"
+          alt="Foto do usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Rodrigo Gonçalves</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={signOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+};
