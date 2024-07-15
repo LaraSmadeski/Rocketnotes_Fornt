c173f1fa2ac5e1f548a58ac1b7cb61b67cdc2f89
Lara Smadeski <lasmadeski@gmail.com>
2024-07-15 21:54:48 +0100

    4º commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,61 @@
-import { FiPlus } from 'react-icons/fi'
-import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../service/api";
 
-import { Note } from '../../components/Note'
-import { Input } from '../../components/Input'
-import { Header } from '../../components/Header'
-import { Section } from '../../components/Section'
-import { ButtonText } from '../../components/ButtonText'
+import { FiPlus } from "react-icons/fi";
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
+
+import { Note } from "../../components/Note";
+import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
+  const [notes, setNotes] = useState([]);
+
+  const navigate = useNavigate();
+
+  function handleTagSelected(tagName){
+    if(tagName === "all"){
+      return setTagsSelected([]);
+    };
+
+    const alreadySelected = tagsSelected.includes(tagName);
+    
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    } else{
+      setTagsSelected(prevState => [...prevState, tagName]);
+    };
+  };
+
+  function handleDetails(id){
+    navigate(`/details/${id}`);
+  };
+
+  useEffect(() => {
+    async function fetchTags(){
+      const response = await api.get("/tags");
+      setTags(response.data);
+    };
+
+    fetchTags();
+  }, []);
+
+  useEffect(() => {
+    async function fetchNotes(){
+      const response = await api.get(`/notes?title=${search}&tags=${tagsSelected}`);
+      setNotes(response.data);
+    };
+
+    fetchNotes();
+  }, [tagsSelected, search]);
+
   return (
     <Container>
       <Brand>
@@ -17,25 +65,44 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+        <li>
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")} 
+            $isactive={tagsSelected.length === 0}
+          />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                $isactive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" />
+        <Input 
+          placeholder="Pesquisar pelo título"
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note data={{
-            title: 'React',
-            tags: [
-              { id: '1', name: 'react' },
-              { id: '2', name: 'rocketseat' }
-            ]
-          }}
-          />
+          {
+            notes.map(note => (
+              <Note
+                key={String(note.id)}
+                data={note}
+                onClick={() => handleDetails(note.id)}
+              />
+            ))
+          }
         </Section>
       </Content>
 
@@ -44,5 +111,5 @@ export function Home() {
         Criar nota
       </NewNote>
     </Container>
-  )
-}
+  );
+};
